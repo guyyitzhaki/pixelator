299303958b3256003e055a26214229343b8bf1e8
guyyitzhaki <guy.yitzhaki@gmail.com>
2014-04-29 13:18:06 +0300

    cleanup

--- DarkCam/src/ofApp.cpp
+++ DarkCam/src/ofApp.cpp
@@ -1,30 +1,32 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
     showControls = true;
     fullscreen = false;
     setFullScreen();
     ofBackground(ofColor::black);
 
-	// enable depth->video image calibration?
-	kinect.setRegistration(false);
+    // enable depth->video image calibration?
+    kinect.setRegistration(false);
 
-	// turn off video cam to save cpu
+    // turn off video cam to save cpu
     kinect.init(false, true);
 
     kinect.open();
 
-	depthImage.allocate(kinect.width, kinect.height);
+    depthImage.allocate(kinect.width, kinect.height);
 
-	ofSetFrameRate(60);
+    ofSetFrameRate(60);
 
-	loadSettings();
+    loadSettings();
 
 
 }
 
-void ofApp::setFullScreen() {
+void ofApp::setFullScreen()
+{
     ofSetFullscreen(fullscreen);
     if (fullscreen)
         ofHideCursor();
@@ -33,34 +35,38 @@ void ofApp::setFullScreen() {
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update()
+{
 
-	kinect.update();
-	if(kinect.isFrameNew()) {
+    kinect.update();
+    if(kinect.isFrameNew())
+    {
 
-		// load grayscale depth image from the kinect source
-		depthImage.setFromPixels(kinect.getDepthPixels(), kinect.width, kinect.height);
+        // load grayscale depth image from the kinect source
+        depthImage.setFromPixels(kinect.getDepthPixels(), kinect.width, kinect.height);
 
-		// we do two thresholds - one for the far plane and one for the near plane
-		// we then do a cvAnd to get the pixels which are a union of the two thresholds
+        // we do two thresholds - one for the far plane and one for the near plane
+        // we then do a cvAnd to get the pixels which are a union of the two thresholds
         grayThreshNear = depthImage;
         grayThreshFar = depthImage;
         grayThreshNear.threshold(nearThreshold, true);
         grayThreshFar.threshold(farThreshold);
         cvAnd(grayThreshNear.getCvImage(), grayThreshFar.getCvImage(), depthImage.getCvImage(), NULL);
 
-		// update the cv images
-		depthImage.flagImageChanged();
+        // update the cv images
+        depthImage.flagImageChanged();
 
-		contourFinder.findContours(depthImage, 50, (kinect.width*kinect.height)/2, 2, false);
-	}
+        contourFinder.findContours(depthImage, 50, (kinect.width*kinect.height)/2, 2, false);
+    }
 
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-    if (showControls) {
+void ofApp::draw()
+{
+    if (showControls)
+    {
         ofNoFill();
 
         ofSetColor(ofColor::white);
@@ -88,18 +94,20 @@ void ofApp::draw(){
         stringstream reportStream;
 
         reportStream << "near threshold " << nearThreshold << " (press: + -)" << endl
-        << "far threshold " << farThreshold << " (press: < >)" << endl
-        << "num blobs found " << contourFinder.nBlobs << endl
-        << "fps: " << ofGetFrameRate() << endl
-        << "press c to close the connection and o to open it again, connection is: " << kinect.isConnected() << endl;
+                     << "far threshold " << farThreshold << " (press: < >)" << endl
+                     << "num blobs found " << contourFinder.nBlobs << endl
+                     << "fps: " << ofGetFrameRate() << endl
+                     << "press c to close the connection and o to open it again, connection is: " << kinect.isConnected() << endl;
 
         reportStream << "angle: " << angle << " degrees (UP and DOWN)" << endl
-        << "0-5 to change the led mode" << endl;
+                     << "0-5 to change the led mode" << endl;
 
         ofDrawBitmapString(reportStream.str(), 20, 652);
     }
-    else {
-        for(int i = 0; i < contourFinder.nBlobs; i++) {
+    else
+    {
+        for(int i = 0; i < contourFinder.nBlobs; i++)
+        {
             ofRectangle r = contourFinder.blobs.at(i).boundingRect;
             r.x = ofMap(r.x, 0, kinect.width, 0, ofGetWidth());
             r.y = ofMap(r.y, 0, kinect.height, 0, ofGetHeight());
@@ -120,12 +128,14 @@ void ofApp::draw(){
 
 }
 
-void ofApp::exit() {
-	kinect.setCameraTiltAngle(0); // zero the tilt on exit
-	kinect.close();
+void ofApp::exit()
+{
+    kinect.setCameraTiltAngle(0); // zero the tilt on exit
+    kinect.close();
 }
 
-void ofApp::saveSettings() {
+void ofApp::saveSettings()
+{
     ofxXmlSettings settings;
     settings.setValue("settings:nearThreshold", nearThreshold);
     settings.setValue("settings:farThreshold", farThreshold);
@@ -134,140 +144,151 @@ void ofApp::saveSettings() {
     settings.saveFile("settings.xml");
 }
 
-void ofApp::loadSettings() {
+void ofApp::loadSettings()
+{
     ofxXmlSettings settings;
     settings.loadFile("settings.xml");
     nearThreshold = settings.getValue("settings:nearThreshold", 230);
     farThreshold = settings.getValue("settings:farThreshold", 70);
     angle = settings.getValue("settings:angle", 0);
-	kinect.setCameraTiltAngle(angle);
+    kinect.setCameraTiltAngle(angle);
     bool on = settings.getValue("settings:led", false);
     if (!on)
         kinect.setLed(ofxKinect::LED_OFF);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-	switch (key) {
-		case '>':
-		case '.':
-			farThreshold ++;
-			if (farThreshold > 255) farThreshold = 255;
-			break;
+void ofApp::keyPressed(int key)
+{
+    switch (key)
+    {
+    case '>':
+    case '.':
+        farThreshold ++;
+        if (farThreshold > 255) farThreshold = 255;
+        break;
 
-		case '<':
-		case ',':
-			farThreshold --;
-			if (farThreshold < 0) farThreshold = 0;
-			break;
+    case '<':
+    case ',':
+        farThreshold --;
+        if (farThreshold < 0) farThreshold = 0;
+        break;
 
-		case '+':
-		case '=':
-			nearThreshold ++;
-			if (nearThreshold > 255) nearThreshold = 255;
-			break;
+    case '+':
+    case '=':
+        nearThreshold ++;
+        if (nearThreshold > 255) nearThreshold = 255;
+        break;
 
-		case '-':
-			nearThreshold --;
-			if (nearThreshold < 0) nearThreshold = 0;
-			break;
+    case '-':
+        nearThreshold --;
+        if (nearThreshold < 0) nearThreshold = 0;
+        break;
 
-        case 'w':
-			kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
-			break;
+    case 'w':
+        kinect.enableDepthNearValueWhite(!kinect.isDepthNearValueWhite());
+        break;
 
-		case 's':
-            saveSettings();
-			break;
+    case 's':
+        saveSettings();
+        break;
 
-		case 'l':
-            loadSettings();
-			break;
+    case 'l':
+        loadSettings();
+        break;
 
-		case '1':
-			kinect.setLed(ofxKinect::LED_GREEN);
-			break;
+    case '1':
+        kinect.setLed(ofxKinect::LED_GREEN);
+        break;
 
-		case '2':
-			kinect.setLed(ofxKinect::LED_YELLOW);
-			break;
+    case '2':
+        kinect.setLed(ofxKinect::LED_YELLOW);
+        break;
 
-		case '3':
-			kinect.setLed(ofxKinect::LED_RED);
-			break;
+    case '3':
+        kinect.setLed(ofxKinect::LED_RED);
+        break;
 
-		case '4':
-			kinect.setLed(ofxKinect::LED_BLINK_GREEN);
-			break;
+    case '4':
+        kinect.setLed(ofxKinect::LED_BLINK_GREEN);
+        break;
 
-		case '5':
-			kinect.setLed(ofxKinect::LED_BLINK_YELLOW_RED);
-			break;
+    case '5':
+        kinect.setLed(ofxKinect::LED_BLINK_YELLOW_RED);
+        break;
 
-		case '0':
-			kinect.setLed(ofxKinect::LED_OFF);
-			break;
+    case '0':
+        kinect.setLed(ofxKinect::LED_OFF);
+        break;
 
-		case 'f':
-		    fullscreen = !fullscreen;
-			setFullScreen();
-			break;
+    case 'f':
+        fullscreen = !fullscreen;
+        setFullScreen();
+        break;
 
-		case OF_KEY_UP:
-			angle++;
-			if(angle>30) angle=30;
-			kinect.setCameraTiltAngle(angle);
-			break;
+    case OF_KEY_UP:
+        angle++;
+        if(angle>30) angle=30;
+        kinect.setCameraTiltAngle(angle);
+        break;
 
-		case OF_KEY_DOWN:
-			angle--;
-			if(angle<-30) angle=-30;
-			kinect.setCameraTiltAngle(angle);
-			break;
+    case OF_KEY_DOWN:
+        angle--;
+        if(angle<-30) angle=-30;
+        kinect.setCameraTiltAngle(angle);
+        break;
 
-		case 'c':
-            showControls = !showControls;
-            break;
+    case 'c':
+        showControls = !showControls;
+        break;
 
-	}
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::keyReleased(int key)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y )
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mousePressed(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mouseReleased(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
+void ofApp::windowResized(int w, int h)
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg)
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 
 }
--- DarkCam/src/ofApp.h
+++ DarkCam/src/ofApp.h
@@ -5,44 +5,45 @@
 #include "ofxKinect.h"
 #include "ofxXmlSettings.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp
+{
 
-	public:
-		void setup();
-		void update();
-		void draw();
-		void exit();
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-        void saveSettings();
-        void loadSettings();
-        void setFullScreen();
+    void saveSettings();
+    void loadSettings();
+    void setFullScreen();
 
-		ofxKinect kinect;
-        ofxCvGrayscaleImage depthImage; // grayscale depth image
-        ofxCvContourFinder contourFinder;
+    ofxKinect kinect;
+    ofxCvGrayscaleImage depthImage; // grayscale depth image
+    ofxCvContourFinder contourFinder;
 
-        ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
-        ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+    ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
+    ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
 
 
-        int nearThreshold;
-        int farThreshold;
+    int nearThreshold;
+    int farThreshold;
 
-        int angle;
-        bool ledOn;
+    int angle;
+    bool ledOn;
 
-        bool showControls;
-        bool fullscreen;
+    bool showControls;
+    bool fullscreen;
 
 
 };
