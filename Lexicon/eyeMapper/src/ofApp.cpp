de5c6c08c58a4754bc0732daf8c818d752bfaf7f
guyyitzhaki <guy.yitzhaki@gmail.com>
2014-05-13 08:18:31 +0300

    moved players to separate class

--- Lexicon/eyeMapper/src/ofApp.cpp
+++ Lexicon/eyeMapper/src/ofApp.cpp
@@ -1,5 +1,60 @@
 #include "ofApp.h"
 
+VideoPart::VideoPart() {
+}
+
+VideoPart::~VideoPart() {
+}
+
+void VideoPart::init(string path, int _w, int _h) {
+    w = _w;
+    h = _h;
+    play(path);
+}
+
+void VideoPart::play(string path) {
+    player.loadMovie(path);
+    player.setSpeed(1.0);
+    player.play();
+}
+
+void VideoPart::update()  {
+    if (targetx != x) {
+        if (targetx > x)
+            x++;
+        else
+            x--;
+    }
+    if (targety != y) {
+        if (targety > y)
+            y++;
+        else
+            y--;
+    }
+    player.update();
+}
+
+void VideoPart::draw()  {
+    player.draw(x, y, w, h);
+}
+
+void VideoPart::place(int _x, int _y) {
+    x = _x;
+    y = _y;
+    targetx = x;
+    targety = y;
+}
+
+void VideoPart::switchTo(string path) {
+    player.closeMovie();
+    play(path);
+}
+
+void VideoPart::setTarget(int _x, int _y) {
+    targetx = _x;
+    targety = _y;
+}
+
 //--------------------------------------------------------------
 void ofApp::setup()
 {
@@ -9,8 +64,8 @@ void ofApp::setup()
     ofBackground(50);
     lastSwitch = ofGetElapsedTimef();
     switchEvery = 10;
-    sliceWidth  = 72;
-    sliceHeight = 64;
+    sliceWidth  = 64;
+    sliceHeight = 48;
     loadVideos();
 
     _mapping = new ofxMtlMapping2D();
@@ -23,7 +78,8 @@ void ofApp::update()
 {
     _mapping->update();
     for (int i = 0; i < NUM_VIDEOS; i++) {
-        videos[i].update();
+  //      videos[i].update();
+        parts[i].update();
     }
     if (ofGetElapsedTimef() - lastSwitch > switchEvery)
     {
@@ -42,18 +98,8 @@ void ofApp::draw()
         ofBackground(0);
         ofSetColor(255);
 
-        int xOffset = 0;
-        int yOffset = 0;
-
-        for (int i = 0; i < NUM_VIDEOS; i++)
-        {
-            videos[i].draw(xOffset,yOffset, sliceWidth,sliceHeight);
-            xOffset += sliceWidth;
-            if (xOffset >= ofGetWidth())
-            {
-                xOffset = 0;
-                yOffset += sliceHeight;
-            }
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            parts[i].draw();
         }
 
         _mapping->unbind();
@@ -67,10 +113,19 @@ void ofApp::loadVideos()
     loaded = !(eyes.size() == 0);
     if (loaded)
     {
+        int xOffset = 0;
+        int yOffset = 0;
         for (int i = 0; i < NUM_VIDEOS; i++)
         {
             int idx = (int) ofRandom(eyes.size());
             setupVideo(i, eyes[idx]);
+            parts[i].place(xOffset,yOffset);
+            xOffset += sliceWidth;
+            if (xOffset >= ofGetWidth())
+            {
+                xOffset = 0;
+                yOffset += sliceHeight;
+            }
         }
     }
 }
@@ -100,25 +155,26 @@ string ofApp::getRandomVideo() {
 
 void ofApp::switchVideo()
 {
-    int playeridx = (int) ofRandom(NUM_VIDEOS);
+
+    int idx = (int) ofRandom(NUM_VIDEOS);
     string path = getRandomVideo();
-    videos[playeridx].closeMovie();
-    setupVideo(playeridx, path);
+    parts[idx].switchTo(path);
+    cout << "switched " << idx << " to " << path << endl;
 }
 
 void ofApp::setupVideo(int idx, string path)
 {
     cout << "[" << idx << "] " << path << endl;
-    videos[idx].loadMovie(path);
-    videos[idx].setSpeed(1.0);
-    videos[idx].play();
+    parts[idx].init(path, sliceWidth, sliceHeight);
 }
 
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
-    if (key == 'm') {
+
+    switch(key) {
+    case 'm':
         config = !config;
         if (config) {
             ofShowCursor();
@@ -126,6 +182,14 @@ void ofApp::keyPressed(int key)
         else {
             ofHideCursor();
         }
+        break;
+     case 'x': {
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            parts[i].setTarget(ofRandom(ofGetWidth()),ofRandom(ofGetHeight()));
+        }
+        break;
+
+     }
     }
 }
 
--- Lexicon/eyeMapper/src/ofApp.h
+++ Lexicon/eyeMapper/src/ofApp.h
@@ -6,39 +6,55 @@
 
 #define NUM_VIDEOS 256
 
+class VideoPart {
+public:
+    VideoPart();
+    ~VideoPart();
+    void init(string path, int _w, int _h);
+	void draw();
+	void update();
+	void setTarget(int x, int y);
+	void place(int x, int y);
+    void switchTo(string path);
+private:
+    void play(string path);
+    ofVideoPlayer player;
+	int x, y, w, h;
+	int targetx, targety;
 
-class ofApp : public ofBaseApp
-{
+};
+
+class ofApp : public ofBaseApp {
 
 public:
-    void setup();
-    void update();
-    void draw();
+	void setup();
+	void update();
+	void draw();
 
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-    vector<string> getVideos();
-    string getRandomVideo();
-    void loadVideos();
-    void switchVideo();
+	vector<string> getVideos();
+	string getRandomVideo();
+	void loadVideos();
+	void switchVideo();
 private:
-    bool loaded;
-    ofxMtlMapping2D* _mapping;
-    ofVideoPlayer	videos[NUM_VIDEOS];
-    int sliceWidth, sliceHeight;
-    int lastSwitch;
-    int switchEvery;
-    bool config;
-
-    void setupVideo(int idx, string path);
+	bool loaded;
+	ofxMtlMapping2D* _mapping;
+	VideoPart	parts[NUM_VIDEOS];
+	int sliceWidth, sliceHeight;
+	int lastSwitch;
+	int switchEvery;
+	bool config;
+	void setupVideo(int idx, string path);
 
 
 };
+
